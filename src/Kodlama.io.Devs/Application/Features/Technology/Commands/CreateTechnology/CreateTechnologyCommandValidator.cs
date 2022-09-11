3995e26247184a74b754051070218c5cab0243c4
Samet Akca <developer2@cevizsoft.com>
2022-09-11 14:27:59 +0300

    Validator added

---
+++ src/Kodlama.io.Devs/Application/Features/Technology/Commands/CreateTechnology/CreateTechnologyCommandValidator.cs
@@ -0,0 +1,19 @@
+﻿using Application.Features.Technology.Commands.Create;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Features.Technology.Commands.CreateTechnology
+{
+    public class CreateTechnologyCommandValidator : AbstractValidator<CreateTechnologyCommand>
+    {
+        public CreateTechnologyCommandValidator()
+        {
+            RuleFor(t => t.Name).NotEmpty().MinimumLength(2);    
+            RuleFor(t => t.ProgrammingLanguageId).NotEmpty();    
+        }
+    }
+}
